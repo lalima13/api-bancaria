7f8dce540161e9527e7db27f0629d0c78e4033bd
Lucas Lima <lucaslimatech98@gmail.com>
2026-06-12 23:04:28 -0300

    Arquivos commitados primariamente

--- src/main.py
+++ src/main.py
@@ -1,11 +1,38 @@
-from fastapi import FastAPI
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI, Request, status
 
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+        await database.connect()
+        yield
+        await database.disconnect()
+
+
+tags_metadata = [
+        {
+                "name": "auth",
+                "description": "Operations for authentication.",            
+        },
+        {
+                "name": "account",
+                "description": "Operations to maintain accounts.",       
+        },
+        {
+                "name": "transaction",
+                "description": "Operations to maintain transactions.",        
+        },
+]            
 
 
 
 app = FastAPI(
     title="Transactions API",
     version="1.0",
-    summary="Microservice to maintain withdral and deposit operations from current account"
+    summary="Microservice to maintain withdral and deposit operations from current account",
+    description="Transactions API is the microservice for recording current account transactions.",
+    openapi_tags="tags_metadata",
+    redoc_url="None",
+    lifespan=lifespan,
 ) 
