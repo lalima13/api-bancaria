bee2d61adf1b1511a01e3cab0ba7fca2a9cb95a3
Lucas Lima <lucaslimatech98@gmail.com>
2026-06-16 22:33:43 -0300

    Atualizações na main

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI, Request, status
-
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -36,3 +37,23 @@ app = FastAPI(
     redoc_url="None",
     lifespan=lifespan,
 ) 
+
+app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=["*"],
+        allow_methods=["*"],
+        allow_headers=["*"],
+)
+
+app.include_router(auth.router, tags=["auth"])
+app.include_router(account.router, tags=["account"])
+app.include_router(transaction.router, tags=["transaction"])
+
+@app.exception_handler(AccountNotFoundError)
+async def account_not_found_error_handler(request: Request, exc: AccountNotFoundError):
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"detail": "Account not found."})
+
+@app.exception_handler(BusinessError)
+async def business_error_handle(request: Request, exc: AccountNotFoundError):
+        return JSONResponse(status_code=status.HTTP_409_CONFLICT, content={"detail": str(exc)})
